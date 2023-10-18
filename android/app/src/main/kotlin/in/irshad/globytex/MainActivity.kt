147a6807e730a58d1d9ddf5c0a9010379e4636d8
IrshadAbdulla19 <irshadabdulla19@gmail.com>
2023-10-18 17:17:04 +0530

    new project for globy tex is started

---
+++ android/app/src/main/kotlin/in/irshad/globytex/MainActivity.kt
@@ -0,0 +1,6 @@
+package `in`.irshad.globytex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text("hey"),
+          );
+        },
+      ),
+    );
+  }
+}
